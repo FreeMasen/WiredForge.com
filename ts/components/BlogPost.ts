7921f7d6d901b7ed76f5985336cb22734617a5be
FreeMasen <RFM@Herman.local>
2017-07-04 13:24:50 -0500

    add snippet logic

--- ts/components/BlogPost.ts
+++ ts/components/BlogPost.ts
@@ -6,12 +6,17 @@ export class BlogPost implements Component {
     edit: HTMLElement;
     html = new HTML();
     md = new MDParser();
-    constructor(post:Post, editable: boolean = false) {
+    constructor(post:Post, editable: boolean = false, isInList: boolean = true) {
         let header = this.header(post.title, post.author, post.fbKey, editable);
         this.node = this.html.div(header,
             new Attribute('class', 'blog-post'));
-        let c = this.content(post.content);
-        this.html.addContent(this.node, [c]);
+        if (isInList) {
+            let snippet = this.snippet(post.content, post.fbKey);
+            this.html.addContent(this.node, [snippet]);
+        } else {
+            let c = this.content(post.content);
+            this.html.addContent(this.node, [c]);
+        }
     }
 
     header(title: string, author: string, id: string, editable: boolean): HTMLElement {
@@ -40,11 +45,41 @@ export class BlogPost implements Component {
     content(content: string): HTMLElement {
         let container = this.html.div(null, 
             new Attribute('class', 'content'));
+        var formattedContent = this.formatContent(content);
+
         return this.html.addContent(container, this.formatContent(content));
     }
 
+    snippet(content: string, id: string): HTMLElement {
+        //break the paragraphs into an array
+        var paragraphs = content.split('\n');
+        //if we have 0 or 1 paragraphs, don't limit the post
+        if (paragraphs.length < 2) {
+            return this.content(content);
+        }
+        //The total number of letters
+        var total = 0;
+        //The number of paragraphs to add
+        var numberOfParagraphs = 0;
+        //loop while the total is less than 250 characters
+        while (total < 250 && numberOfParagraphs < paragraphs.length) {
+            //Add the length of the current paragraph
+            total += paragraphs[numberOfParagraphs].length;
+            //increment the number of paragraphs we have captured
+            numberOfParagraphs++;
+        }
+        //create the main container
+        var container = this.content(paragraphs.slice(0, numberOfParagraphs).join('\n'));
+        //create the more button
+        var more = this.html.button('more...', new Attribute('class', 'more-button'),
+                                                new Attribute('id', 'more-' + id));
+        //Add the more button to the container
+        this.html.addContent(container, [more]);
+        return container;
+    }
+
     private formatContent(content: string): HTMLElement[] {
-        if (!content) return;
+        // if (!content) return;
         var split = content.split('\n');
         return split.filter(line => {
             return line.trim().length > 0;
