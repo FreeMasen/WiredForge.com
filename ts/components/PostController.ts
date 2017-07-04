7f520427dfa5e3af2be7ed4b3cfe38bf33e29905
FreeMasen <RFM@Herman.local>
2017-07-04 13:25:45 -0500

    completed first working implementation

--- ts/components/PostController.ts
+++ ts/components/PostController.ts
@@ -5,40 +5,48 @@ import { BlogPost } from '../components';
 export class PostController implements Component {
     private html = new HTML();
     private events = new EventHandler;
+
     private currentPage: number = 0;
     private paginatedPosts: Array<HTMLElement[]> = [];
 
+    private backButton: HTMLButtonElement;
+    private forwardButton: HTMLButtonElement;
 
     node: HTMLElement;
+
+    private posts: HTMLElement;
     constructor(posts: Post[], editable: boolean) {
-        // debugger
         Logger.log('PostController', 'constructor', posts, editable);
         var blogPosts = this.constructPosts(posts, editable);
         this.paginatedPosts = this.paginatePosts(blogPosts);
-        var nodes = this.html.div(this.paginatedPosts[this.currentPage], new Attribute('id', 'posts-wrapper'));
-        this.node = this.html.div([nodes, this.footerToolbar()], new Attribute('id', 'post-controller'));
+        this.posts = this.html.div(this.paginatedPosts[this.currentPage], new Attribute('id', 'posts-wrapper'));
+        this.node = this.html.div([this.posts, this.footerToolbar()], new Attribute('id', 'post-controller'));
+        this.updateButtons();
     }
 
     pageForward(): void {
-        Logger.log('PostController', 'pageForward');
+        Logger.log('PostController', 'pageForward', this.currentPage);
         if (this.currentPage < this.paginatedPosts.length) {
             this.currentPage++;
             this.fillNode(this.paginatedPosts[this.currentPage]);
+            this.updatePageText();
         }
     }
 
     pageBackward(): void {
-        Logger.log('PostController', 'pageBackward');
+        Logger.log('PostController', 'pageBackward', this.currentPage);
         if (this.currentPage > 0) {
             this.currentPage--;
-            this.fillNode(this.paginatedPosts[this.currentPage])
+            this.fillNode(this.paginatedPosts[this.currentPage]);
+            this.updatePageText();
         }
     }
 
     fillNode(nodes: HTMLElement[]): void {
         Logger.log('PostController', 'fillNode', nodes);
-        this.html.clearChildren(this.node);
-        this.html.addContent(this.node, nodes);
+        this.updateButtons();
+        this.html.clearChildren(this.posts);
+        this.html.addContent(this.posts, nodes);
     }
 
     constructPosts(posts: Post[], editable: boolean): BlogPost[] {
@@ -56,11 +64,11 @@ export class PostController implements Component {
         var currentPage: HTMLElement[] = [];
         for (var i = 0; i < posts.length; i++) {
             currentPage.push(posts[i].node);
-            if (currentPage.length >= 10) {
+            if (currentPage.length >= 10
+                || i == posts.length - 1) {
                 ret.push(currentPage.splice(0));
             }
         }
-        ret.push(currentPage.splice(0));
         return ret;
     }
 
@@ -68,12 +76,14 @@ export class PostController implements Component {
         Logger.log('PostController', 'footerToolbar');
         var leftIcon = this.html.i('chevron_left');
         var rightIcon = this.html.i('chevron_right');
-        var leftButton = this.html.button(leftIcon, new Attribute('id', 'back-button'));
-        var rightButton = this.html.button(rightIcon, new Attribute('id', 'forward-button'));
-        this.events.registerNodeEvent(leftButton, '#back-button', 'click', this.pageBackward, this);
-        this.events.registerNodeEvent(rightButton, '#forward-button', 'click', this.pageForward, this);
+        this.backButton = this.html.button(leftIcon, new Attribute('id', 'back-button'),
+                                                    new Attribute('class', 'pagination-button'));
+        this.forwardButton = this.html.button(rightIcon, new Attribute('id', 'forward-button'),
+                                                        new Attribute('class', 'pagination-button'));
+        this.events.registerNodeEvent(this.backButton, '#back-button', 'click', this.pageBackward, this);
+        this.events.registerNodeEvent(this.forwardButton, '#forward-button', 'click', this.pageForward, this);
         var pageIndicator = this.html.span(this.pageText, new Attribute('id', 'page-indicator'));
-        return this.html.div([leftButton, pageIndicator, rightButton], new Attribute('id', 'post-footer-toolbar'))
+        return this.html.div([this.backButton, pageIndicator, this.forwardButton], new Attribute('id', 'post-footer-toolbar'))
     }
 
     updatePageText(): void {
@@ -85,5 +95,18 @@ export class PostController implements Component {
     get pageText(): string {
         Logger.log('PostController', 'pageText');
         return `${this.currentPage + 1} of ${this.paginatedPosts.length}`;
+    }
+
+    updateButtons(): void {
+        if (this.currentPage === 0) {
+            this.backButton.disabled = true;
+        } else {
+            this.backButton.disabled = false;
+        }
+        if (this.currentPage + 1 >= this.paginatedPosts.length) {
+            this.forwardButton.disabled = true;
+        } else {
+            this.forwardButton.disabled = false;
+        }
     }
 }
