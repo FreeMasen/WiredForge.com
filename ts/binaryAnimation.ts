cee19804a05500254bd42a6eb77952bef26d3557
Robert Masen <rmasen@USIHQ.net>
2018-01-09 17:03:16 -0600

    more playing around with animations

--- ts/binaryAnimation.ts
+++ ts/binaryAnimation.ts
@@ -27,10 +27,16 @@ class BinaryAnimation {
     renderDino() {
         this.context.save();
         this.context.beginPath();
-        this.context.strokeStyle = '#000';
         this.context.fillStyle = 'green';
         this.context.arc(360, 150, 75, DrawingService.degToRads(0), DrawingService.degToRads(200), true);
+        this.context.bezierCurveTo(250, 200, 200, 140, 250, 200);
+        this.context.arc(230, 220, 50, DrawingService.degToRads(270), DrawingService.degToRads(1200), true);
+        this.context.bezierCurveTo(240, 260, 240, 250, 240, 250)
+        this.context.bezierCurveTo(235, 245, 230, 270, 220, 270);
+        this.context.bezierCurveTo(440, 250, 290, 380, 320, 400);
+        this.context.bezierCurveTo(260, 450, 400, 450, 300, 500);
+        this.context.lineTo(450, 500);
         this.context.fill();
-        this.context.stroke();
+        this.context.restore();
     }
 }
