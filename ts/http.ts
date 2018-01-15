cf17c19aaf2449ae85c098bc37fb6d6b8b45ac77
FreeMasen <RFM@Herman.local>
2018-01-15 07:43:56 -0600

    added logging

--- ts/http.ts
+++ ts/http.ts
@@ -18,21 +18,28 @@ export default class Http {
     }
 
     static async post<T = any>(url: string, body: string = ''): Promise<T> {
+        console.log('http.post', url, body);
         return new Promise<T>((resolve, reject) => {
             var xhr = new XMLHttpRequest();
             xhr.open('POST', url);
             xhr.onreadystatechange = () => {
                 if (xhr.readyState == xhr.DONE) {
+                    console.log('Response received')
                     try {
                         let res = JSON.parse(xhr.responseText);
+                        console.log('parsed to json');
                         resolve(res);
                     } catch (e) {
+                        console.log('failed to parse', xhr.responseText);
                         resolve(xhr.responseText as any);
                     }
                 }
             }
             if (url.substr(url.indexOf('?')).length > 0)
                 xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            else if (body != '')
+                xhr.setRequestHeader('Content-Type', 'application/json');
+            console.log('sending request');
             xhr.send(body);
         });
     }
