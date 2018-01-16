2129913180691a1cacc9709ac0c34d99859482ea
Robert Masen <rmasen@USIHQ.net>
2018-01-16 17:04:46 -0600

    add second config to allow for localhost:1111 to be dev base url

--- refresh.sh
+++ refresh.sh
@@ -3,6 +3,6 @@ cd "$(dirname "$0")"
 git pull
 #npm i
 webpack --env prod
-gutenberg build
+gutenberg build --base-url https://wiredforge.com
 
 #cargo build --release & ~/projects/wiredforge.com/server/target/release/server &
