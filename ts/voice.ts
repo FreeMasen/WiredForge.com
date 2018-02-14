6669f338db40da5921d03ce4208138ba914fab07
Robert Masen <rmasen@USIHQ.net>
2018-02-14 17:04:13 -0600

    add Speaker abstraction

--- ts/voice.ts
+++ ts/voice.ts
@@ -16,11 +16,91 @@ window.addEventListener('DOMContentLoaded', () => {
         speechSynthesis.speak(new SpeechSynthesisUtterance(''));
     }
 });
+type EventHandler = (ev: Event) => void;
+class Speaker extends EventTarget {
+    private _eventTarget: EventTarget;
+    private _speaker: SpeechSynthesis = window.speechSynthesis;
+    private handlers = new Map<string, Array<EventHandler>>();
+    constructor() {
+        super();
+        this._speaker.addEventListener('voiceschanged', ev => this.dispatchEvent(ev))
+    }
+    public addEventListener(event: string, handler: EventHandler) {
+        let existingList = this.handlers.get(event);
+        if (!existingList) {
+            existingList = [];
+        }
+        existingList.push(handler);
+        this.handlers.set(event, existingList);
+    }
+
+    public removeEventListener(event: 'voiceschanged' | 'speaking' | 'stoppedSpeaking' | 'paused' | 'resumed' | 'cancelled', handler: EventHandler) {
+        let existingList = this.handlers.get(event);
+        if (!existingList) return;
+        let index = existingList.findIndex(f => f == handler);
+        if (index < 0) return;
+        existingList.splice(index, 1);
+        this.handlers.set(event, existingList);
+    }
+
+    public dispatchEvent(ev: Event): boolean {
+        for (let h of this.handlers.get(ev.type)) {
+            h(ev);
+            if (ev.defaultPrevented) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public speak(utterance: SpeechSynthesisUtterance) {
+        this.dispatchEvent(new Event('speaking'));
+        this._speaker.speak(utterance);
+        this.watchSpeaker();
+    }
+
+    private watchSpeaker() {
+        if (this._speaker.speaking)
+            return setTimeout(() => this.watchSpeaker(), 0);
+        this.dispatchEvent(new Event('stoppedSpeaking'));
+    }
+
+    public pause() {
+        this.dispatchEvent(new Event('paused'));
+        this._speaker.pause();
+    }
+
+    public resume() {
+        this.dispatchEvent(new Event('resumed'));
+        this._speaker.resume();
+    }
+    public cancel() {
+        this.dispatchEvent(new Event('cancelled'));
+        this._speaker.cancel();
+    }
+
+    public getVoices(): Array<SpeechSynthesisVoice> {
+        return this._speaker.getVoices();
+    }
+
+    get paused(): boolean {
+        return this._speaker.paused;
+    }
+
+    get speaking(): boolean {
+        return this._speaker.speaking;
+    }
+
+    get pending(): boolean {
+        return this._speaker.pending;
+    }
+
+}
 
 class Voice {
     private queue: Array<Statement> = [];
     private discard: Array<Statement> = [];
-    private speaker: SpeechSynthesis = window.speechSynthesis;
+    private speaker: Speaker = new Speaker();
     private stopped: boolean = false;
     private data: Data;
 
@@ -58,6 +138,11 @@ class Voice {
         clear.addEventListener('click', ev => this.clearBox(ev));
         let submit = document.getElementById('submit') as HTMLButtonElement;
         submit.addEventListener('click', ev => this.addUtterance(ev))
+        this.speaker.addEventListener('speaking', ev => console.log('speaking', ev));
+        this.speaker.addEventListener('stoppedSpeaking', ev => console.log('stoppedSpeaking', ev));
+        this.speaker.addEventListener('paused', ev => console.log('paused', ev));
+        this.speaker.addEventListener('cancelled', ev => console.log('cancelled', ev));
+        this.speaker.addEventListener('resumed', ev => console.log('resumed', ev));
     }
 
     /**Add a new statement to the queue */
@@ -72,20 +157,22 @@ class Voice {
             this.selectedVoice.name,
             this.selectedVoice.lang,
         )
-        this.data.upsertStatement(statement)
-        .then(_ => {
-            console.log('statement upserted');
-            this.refreshQueues().then(_ => {
-                console.log('queues refreshed');
-            })
-            .catch(e => {
-                console.error('error refreshing queues', e);
-            });
-        })
-        .catch(e => {
-            console.error('error upserting statement', e);
-        });
-        if (!this.stopped) this.clearQueue();
+        // this.data.upsertStatement(statement)
+        // .then(_ => {
+        //     console.log('statement upserted');
+        //     this.refreshQueues().then(_ => {
+        //         console.log('queues refreshed');
+        //         this.clearQueue();
+        //     })
+        //     .catch(e => {
+        //         console.error('error refreshing queues', e);
+        //     });
+        // })
+        // .catch(e => {
+        //     console.error('error upserting statement', e);
+        // });
+        this.queue.push(statement);
+        this.sayNext();
     }
 
     /**Refresh the data in the queue and discard arrays */
@@ -124,8 +211,14 @@ class Voice {
         }
     }
 
+    sayNext(ev: Event = null) {
+        let s = this.queue.shift();
+        this.speaker.speak(s.toUtterance());
+        this.discard.push(s);
+    }
+
     requeue(s: Statement) {
-        s.lastSpoken = undefined;
+        s.lastSpoken = null;
         this.data.upsertStatement(s);
     }
 
@@ -140,7 +233,8 @@ class Voice {
     }
 
     clearQueue() {
-        if (this.queue.length > 0 && !this.speakerFree) {
+        console.log('clearQueue', this.queue.length, !this.speakerFree);
+        if (this.queue.length > 0 && this.speakerFree) {
             let next = this.queue.shift();
             this.speaker.speak(next.toUtterance());
             next.lastSpoken = new Date();
@@ -152,6 +246,8 @@ class Voice {
     }
 
     get speakerFree(): boolean {
+        console.log('speakerFree', this.speaker.speaking, this.speaker.paused, this.speaker.pending)
+        this.speaker.dispatchEvent(new Event('finished', {}))
         return !this.speaker.speaking && 
                 !this.speaker.paused && 
                 !this.speaker.pending
