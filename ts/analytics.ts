2611edf5630a41b9b046443159c907cc581e3f3c
rfm <r@robertmasen.pizza>
2019-02-04 09:15:22 -0600

    use new analytics

--- ts/analytics.ts
+++ ts/analytics.ts
@@ -1,6 +1,9 @@
-import { sendInfo, sendExiting, setup_click_watcher, initialResponseHandler } from '../../analytics/analytics';
+import { sendInfo, sendExiting, setup_click_watcher, initialResponseHandler, notifyUser, notificationNeeded } from '../../analytics/analytics';
 
 window.addEventListener('load', () => {
+    if (notificationNeeded()) {
+        notifyUser();
+    }
     sendInfo().then(initialResponseHandler);
 });
 
